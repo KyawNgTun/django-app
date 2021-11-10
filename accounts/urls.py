860340e4a66616defe1b3ea398ccab0bb0d83824
kyawnaingtun <kyawnaingtun1992.knt@gmail.com>
2021-11-10 11:31:40 +0630

    order_create/update/delete

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('',views.dashboard,name='dashboard'),
     path('customer/<int:id>',views.customer,name='customer_show'),
     path('product/',views.product,name='product'),
+    path('order/create/',views.orderCreate,name='order.create'),
+    path('order/update/<int:orderId>',views.orderUpdate,name='order.update'),
+    path('order/delete/<int:orderId>',views.orderDelete,name='order.delete'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.db.models.query import InstanceCheckMeta
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
-
 from accounts.models import *
+from .forms import *
 
 def dashboard(request):
     customers=Customer.objects.all()
@@ -31,4 +32,39 @@ def product(request):
     products=Product.objects.all()
     return render(request,'accounts/product.html',{
         'products':products
+    })
+
+
+def orderCreate(request):
+    form=OrderForm()
+    if request.method=="POST":
+        form=OrderForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('/')
+
+    return render(request,'accounts/order_form.html',{
+        'form':form
+    })
+
+def orderUpdate(request,orderId):
+    order=Order.objects.get(id=orderId)
+    form=OrderForm(instance=order)
+    if request.method=="POST":
+        form=OrderForm(request.POST,instance=order)
+        if form.is_valid:
+            form.save()
+            return redirect('/')
+
+    return render(request,'accounts/order_form.html',{
+        'form':form
+    })
+
+def orderDelete(request,orderId):
+    order=Order.objects.get(id=orderId)
+    if request.method=="POST":
+        order.delete()
+        return redirect('/')
+    return render(request,'accounts/order_delete.html',{
+        'order':order
     })
